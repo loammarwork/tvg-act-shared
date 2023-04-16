853ef9d52573b3448b42b069bcef826b736332bc
Lodev <lodev.ceo@gmail.com>
2023-04-17 06:05:56 +0700

    add description mapper

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,3 @@
 export { SearchStandardRequestDTO } from './dto/search/search.standard.request.dto';
 export { SearchStandardResponseDTO } from './dto/search/search.standard.response.dto';
+export { DescriptionMapper } from './mapper/description-mapper';
--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.SearchStandardResponseDTO = exports.SearchStandardRequestDTO = void 0;
+exports.DescriptionMapper = exports.SearchStandardResponseDTO = exports.SearchStandardRequestDTO = void 0;
 var search_standard_request_dto_1 = require("./dto/search/search.standard.request.dto");
 Object.defineProperty(exports, "SearchStandardRequestDTO", { enumerable: true, get: function () { return search_standard_request_dto_1.SearchStandardRequestDTO; } });
 var search_standard_response_dto_1 = require("./dto/search/search.standard.response.dto");
 Object.defineProperty(exports, "SearchStandardResponseDTO", { enumerable: true, get: function () { return search_standard_response_dto_1.SearchStandardResponseDTO; } });
+var description_mapper_1 = require("./mapper/description-mapper");
+Object.defineProperty(exports, "DescriptionMapper", { enumerable: true, get: function () { return description_mapper_1.DescriptionMapper; } });
 //# sourceMappingURL=index.js.map
