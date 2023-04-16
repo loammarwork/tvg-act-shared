de065c3f4913f0f941c60b654dd7b50a24f0b3c8
Lodev <lodev.ceo@gmail.com>
2023-04-17 06:06:45 +0700

    add description mapper and build

---
+++ dist/mapper/description-mapper.d.ts
@@ -0,0 +1,2 @@
+export type DescriptionMapperType = 'How to redeem' | 'Description' | 'Highlights' | 'Overview' | 'Aligibility' | 'Usage Validity' | 'Additional Info' | 'Highlights' | 'Questions' | 'Addition Detail' | 'Ticket Type' | 'Ticket Validity' | 'Excludes' | 'Includes' | 'Important Info' | 'Advanced Tips' | 'Redeem Info' | 'Direct Entrance';
+export declare const DescriptionMapper: Record<string, DescriptionMapperType>;
---
+++ dist/mapper/description-mapper.js
@@ -0,0 +1,26 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.DescriptionMapper = void 0;
+exports.DescriptionMapper = {
+    how_to_redeem: 'How to redeem',
+    description: 'Description',
+    highlight: 'Highlights',
+    overview: 'Overview',
+    aligibility: 'Aligibility',
+    usageValidity: 'Usage Validity',
+    additionalInfo: 'Additional Info',
+    highlights: 'Highlights',
+    inclusions: 'Includes',
+    howToUse: 'How to redeem',
+    questions: 'Questions',
+    operatingHours: 'Addition Detail',
+    ticketFormat: 'Ticket Type',
+    ticketValidity: 'Ticket Validity',
+    excluded: 'Excludes',
+    included: 'Includes',
+    importantInfo: 'Important Info',
+    advancedTips: 'Advanced Tips',
+    redeemInfo: 'Redeem Info',
+    directEntrance: 'Direct Entrance',
+};
+//# sourceMappingURL=description-mapper.js.map
