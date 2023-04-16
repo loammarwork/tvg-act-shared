d5e740c269c188899f5e4568de7c85884feb53a3
Lodev <lodev.ceo@gmail.com>
2023-04-16 22:31:33 +0700

    add response standard in index.ts file export

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,2 @@
 export { SearchStandardRequestDTO } from './dto/search/search.standard.request.dto';
+export { SearchStandardResponseDTO } from './dto/search/search.standard.response.dto';
--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.SearchStandardRequestDTO = void 0;
+exports.SearchStandardResponseDTO = exports.SearchStandardRequestDTO = void 0;
 var search_standard_request_dto_1 = require("./dto/search/search.standard.request.dto");
 Object.defineProperty(exports, "SearchStandardRequestDTO", { enumerable: true, get: function () { return search_standard_request_dto_1.SearchStandardRequestDTO; } });
+var search_standard_response_dto_1 = require("./dto/search/search.standard.response.dto");
+Object.defineProperty(exports, "SearchStandardResponseDTO", { enumerable: true, get: function () { return search_standard_response_dto_1.SearchStandardResponseDTO; } });
 //# sourceMappingURL=index.js.map
