5574f41a8b00ee9c3b2bc30f9191e9e1b7e46668
Lodev <lodev.ceo@gmail.com>
2023-04-17 06:05:34 +0700

    add description-mapper

---
+++ src/mapper/description-mapper.ts
@@ -0,0 +1,50 @@
+export type DescriptionMapperType =
+  | 'How to redeem'
+  | 'Description'
+  | 'Highlights'
+  | 'Overview'
+  | 'Aligibility'
+  | 'Usage Validity'
+  | 'Additional Info'
+  | 'Highlights'
+  | 'Questions'
+  | 'Addition Detail'
+  | 'Ticket Type'
+  | 'Ticket Validity'
+  | 'Excludes'
+  | 'Includes'
+  | 'Important Info'
+  | 'Advanced Tips'
+  | 'Redeem Info'
+  | 'Direct Entrance';
+
+export const DescriptionMapper: Record<string, DescriptionMapperType> = {
+  /* Extranet */
+  how_to_redeem: 'How to redeem',
+  description: 'Description',
+  highlight: 'Highlights',
+  overview: 'Overview',
+  aligibility: 'Aligibility',
+  usageValidity: 'Usage Validity',
+  additionalInfo: 'Additional Info',
+  /* Extranet */
+
+  /* GlobalTix */
+  highlights: 'Highlights',
+  inclusions: 'Includes',
+  howToUse: 'How to redeem',
+  questions: 'Questions',
+  operatingHours: 'Addition Detail',
+  ticketFormat: 'Ticket Type',
+  ticketValidity: 'Ticket Validity',
+  /* GlobalTix */
+
+  /* HotelBeds */
+  excluded: 'Excludes',
+  included: 'Includes',
+  importantInfo: 'Important Info',
+  advancedTips: 'Advanced Tips',
+  redeemInfo: 'Redeem Info',
+  directEntrance: 'Direct Entrance',
+  /* HotelBeds */
+} as const;
