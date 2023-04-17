44220680854c51f44947c0a9af294052a1f05ea0
Lodev <lodev.ceo@gmail.com>
2023-04-17 23:55:09 +0700

    export detail response

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { SearchStandardRequestDTO } from './dto/search/search.standard.request.dto';
 export { SearchStandardResponseDTO } from './dto/search/search.standard.response.dto';
 export { DetailStandardRequestDTO } from './dto/detail/detail.standard.request.dto';
+export { DetailStandardResponseDTO } from './dto/detail/detail.standard.response.dto';
 export { DescriptionMapper } from './mapper/description.mapper';
 export { SegmentMapper } from './mapper/segment.mapper';
